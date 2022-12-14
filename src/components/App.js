109c9b9e27faa6c08e83a0a28b2b77a83c02992e
sahilgarg9911 <105276593+sahilgarg9911@users.noreply.github.com>
2022-12-14 08:08:06 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,23 @@ import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
-//code here 
-const [setColor] = useState("red");
-
+const [value,setValue]=useState(true)
+const [className,setClassName]=useState('redColor')
+const onButtonClick=()=>{
+  setValue(!value);
+}
+useEffect(()=>{
+  if(value){
+    setClassName('redColor')
+  }
+  if(!value){
+    setClassName('blueColor')
+  }
+},[value])
   return (
     <div id="main">
-      <p className={'redColor'} >Newton School</p>
-      <button id='button' onClick={() => setColor('blue')}>Change Style</button>
+      <p className={className} >Newton School</p>
+      <button id='button' onClick={onButtonClick}>Change Style</button>
     </div>
   )
 }
